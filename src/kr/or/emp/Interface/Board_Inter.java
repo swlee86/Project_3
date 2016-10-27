a6d03b05de7a45c0125050cab20570709e20915a
Lee_Sang_Won <airless86@hanmail.net>
2016-10-27 16:11:31 +0900

    Controller 수정 및 Interface 추가 /index.bak(작업을 위해 원본 보관) 추가

---
+++ src/kr/or/emp/Interface/Board_Inter.java
@@ -0,0 +1,9 @@
+package kr.or.emp.Interface;
+
+public interface Board_Inter {
+	String pageview();
+	String detailview();
+	String insert();
+	String update();
+	String delete();
+}
--- src/kr/or/epm/Controller/IndexController.java
+++ src/kr/or/epm/Controller/IndexController.java
@@ -8,20 +8,12 @@ import kr.or.emp.Interface.ViewSubmit_Inter;
 //index.do 접근시에 index.jsp를 열어주는 컨트롤러
 
 @Controller
-public class IndexController implements ViewSubmit_Inter{
+public class IndexController{
 
 	
 	//최초 접속(index.html)시 views/index.jsp 구동
-	@Override
 	@RequestMapping("/index.do")
 	public String view() {
 		return "/views/index";
 	}
-
-	@Override
-	public String submit() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
