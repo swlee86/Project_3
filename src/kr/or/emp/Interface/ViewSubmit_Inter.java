0b4667b7fdaa7db08f3979a86393e371611f03b1
LEE-PC\Lee <airless86@hanmail.net>
2016-10-27 11:35:58 +0900

    jar 파일 추가 및 인터페이스 추가

---
+++ src/kr/or/emp/Interface/ViewSubmit_Inter.java
@@ -0,0 +1,6 @@
+package kr.or.emp.Interface;
+
+public interface ViewSubmit_Inter {
+	String view();
+	String 
+}
---
+++ src/kr/or/epm/Controller/IndexController.java
@@ -0,0 +1,8 @@
+package kr.or.epm.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+}
