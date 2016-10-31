3e57540ab58b28707dbab2d2bec21be5e07c2e30
박성준 <stpark89@naver.com>
2016-10-31 11:22:39 +0900

    주말  뷰 작업본

--- src/main/java/kr/or/epm/LoginController/LoginController.java
+++ src/main/java/kr/or/epm/LoginController/LoginController.java
@@ -2,6 +2,7 @@ package kr.or.epm.LoginController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /*
  * 작성일 : 2016-10-27
@@ -14,6 +15,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+	@RequestMapping(value="/Login.do", method=RequestMethod.GET)
+	public String Login(){
+		System.out.println("로그인");
+		return "login/Login";
+	}
+	
+	@RequestMapping(value="/Login.do", method=RequestMethod.POST)
+	public String LoginResult(String username){
+		System.out.println("로그인 리절트 : "+username);
+		if(username.equals("admin")){
+			return "index";
+		}else{
+			return null;
+		}
+		
+	}
+		
 	@RequestMapping("/AddMember.do")
 	public String AddMember(){
 		System.out.println("회원 가입");
---
+++ src/main/webapp/sidebar/Footer.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <!-- Footer-->
+    <footer class="footer">
+        <span class="pull-right">
+            Example text
+        </span>
+        Company 2015-2020
+    </footer>
+    
