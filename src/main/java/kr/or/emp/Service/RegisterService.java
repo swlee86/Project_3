b6cf98fee66ccd9abde004e0a944a4bedc539ba6
이상원 <airless86@hanmail.net>
2016-11-16 15:56:58 +0900

    1. 회원 가입 수정 - 사번 확인 부분에서 사번 확인 버튼 클릭시 emp 테이블에서 사번 존재 여부 비교 로직 추가 - 사번 존재 유무 확인 후 존재하는 경우 emp_detail을 통해 아이디 존재 여부 비교 로직 추가 - 해당 로직 모두 처리 후 회원가입 화면단에서 결과 값을 뿌려 줌

--- src/main/java/kr/or/emp/Service/RegisterService.java
+++ src/main/java/kr/or/emp/Service/RegisterService.java
@@ -4,17 +4,26 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.or.epm.DAO.RegisterDAO;
 import kr.or.epm.VO.Emp;
 
 
 @Service
 public class RegisterService {
 
-	 @Autowired
-	 private SqlSession sqlsession;
-
+	@Autowired
+	private SqlSession sqlsession;
 	
-	public Emp selectEmpId(int emp){
-		return null;
+	//회원가입시 사원번호 입력 후 체크를 누르면 사원 정보를 불러오는 함수
+	public String selectEmpId(String emp_no){
+		RegisterDAO registerDao = sqlsession.getMapper(RegisterDAO.class);
+		String resultemp = registerDao.selectEmpno(emp_no);
+		return resultemp;
+	}
+	
+	public String selectUsedEmpno(String result){
+		RegisterDAO registerDao = sqlsession.getMapper(RegisterDAO.class);
+		String resultemp = registerDao.selectUsedEmpno(result);
+		return resultemp;
 	}
 }
--- src/main/java/kr/or/epm/DAO/RegisterDAO.java
+++ src/main/java/kr/or/epm/DAO/RegisterDAO.java
@@ -1,10 +1,9 @@
 package kr.or.epm.DAO;
 
-import kr.or.epm.VO.Emp;
 import kr.or.epm.VO.Emp_detail;
 
 public interface RegisterDAO {
 	int insertEmp_detail(Emp_detail emp_detail);
-	Emp selectEmpId(int Emp_no);
-	
+	String selectEmpno(String Emp_no);
+	String selectUsedEmpno(String Emp_no);
 }
