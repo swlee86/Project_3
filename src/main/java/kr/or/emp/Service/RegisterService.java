3781f21097d07af6e05e07a8855b3204017b8283
이상원 <airless86@hanmail.net>
2016-11-16 13:41:49 +0900

    1. registerDao.java 수정 2. RegisterDAO.xml 수정 3. RegisterService 추가

---
+++ src/main/java/kr/or/emp/Service/RegisterService.java
@@ -0,0 +1,14 @@
+package kr.or.emp.Service;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.epm.VO.Emp;
+
+@Service
+public class RegisterService {
+
+	public Emp selectEmpId(int emp){
+		
+		return null;
+	}
+}
--- src/main/java/kr/or/epm/DAO/RegisterDAO.java
+++ src/main/java/kr/or/epm/DAO/RegisterDAO.java
@@ -1,8 +1,10 @@
 package kr.or.epm.DAO;
 
+import kr.or.epm.VO.Emp;
 import kr.or.epm.VO.Emp_detail;
 
 public interface RegisterDAO {
 	public int insertEmp_detail(Emp_detail emp_detail);
+	public Emp selectEmpId(int Emp_no);
 	
 }
