5d5023cae9a7ec382dabce1d77723a84e3e39905
jieun <wong_303@naver.com>
2016-11-18 14:27:18 +0900

    1. 미디어게시판 기능

--- src/main/java/kr/or/epm/BoardController/MediaBoardController.java
+++ src/main/java/kr/or/epm/BoardController/MediaBoardController.java
@@ -1,11 +1,15 @@
 package kr.or.epm.BoardController;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.or.epm.Service.MediaBoardService;
+import kr.or.epm.VO.MediaBoard;
 
 
 /*
@@ -21,22 +25,53 @@ public class MediaBoardController {
 	private MediaBoardService mediaboardservice;
 
 	// 언론게시판 > 언론게시판 리스트 페이지 이동
-	@RequestMapping(value = "/media_board_list.do", method = RequestMethod.GET)
-	public String media_board_list() {
+	@RequestMapping(value = "/media_board_list.do")
+	public String media_board_list(String pg , String f , String q , Model model) {
 		System.out.println("media_board_list() 컨트롤러 탐");
 		
+		int totalcount = 0;
+		int cpage = 1;
+		int pagecount = 0;
+		int pagesize = 5;
+		
+		String field = "title";
+		String query ="%%";
+		
+		List<MediaBoard> list = null;
+		
+		
+		if(pg != null && pg.equals("")){
+			cpage = Integer.parseInt(pg);
+		}
+		if(f != null && f.equals("")){
+			field = f;
+		}
+		if(q != null && q.equals("")){
+			query = q;
+		}
+		
+	
+		totalcount = mediaboardservice.selectCount(field, query);
+
+		System.out.println("cpage:"+cpage+"/ field:"+field+"/ query:"+query+ "/ totalcount:"+totalcount);
+		
+	    if(totalcount % pagesize == 0){       
+	    	pagecount = totalcount/pagesize;
+        }else{
+        	pagecount = (totalcount/pagesize) + 1;
+        }
 		
 		
 		
+
 		
 		
+		list = mediaboardservice.selectList(cpage, pagesize, field, query);
 		
-		
-		
-		
-		
-		
-		
+		model.addAttribute("pagecount", pagecount);
+		model.addAttribute("cpage",cpage);
+		model.addAttribute("totalcount",totalcount);
+		model.addAttribute("list", list);
 		return "board_media.media_board_list";
 		
 	}
--- src/main/java/kr/or/epm/Service/MediaBoardService.java
+++ src/main/java/kr/or/epm/Service/MediaBoardService.java
@@ -1,9 +1,36 @@
 package kr.or.epm.Service;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import kr.or.epm.DAO.MediaBoardDAO;
+import kr.or.epm.VO.MediaBoard;
 
 @Service
 public class MediaBoardService {
+	@Autowired
+	private SqlSession sqlSession;
 
+	// MediaBoard의 리스트를 불러오는 함수
+	public List<MediaBoard> selectList(int cpage, int pagesize, String field, String query) {
+		int start = cpage * pagesize - (pagesize - 1);
+		int end = cpage * pagesize;
+		System.out.println(start + " / " + end);
 
+		MediaBoardDAO mediaBoardDAO = sqlSession.getMapper(MediaBoardDAO.class);
+		List<MediaBoard> list = mediaBoardDAO.selectList(cpage, pagesize, field, query);
+		return list;
+	}
+
+	//전체 게시글 수
+	public int selectCount(String field, String query) {
+		int totalcount = 0;
+		MediaBoardDAO mediaBoardDAO = sqlSession.getMapper(MediaBoardDAO.class);
+		totalcount = mediaBoardDAO.selectCount(field, query);
+		System.out.println("=>  게시글 총 수 : "+ totalcount);
+		return totalcount;
+	}
 }
