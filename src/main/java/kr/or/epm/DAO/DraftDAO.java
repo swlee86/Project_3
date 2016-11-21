508967baf73d429d6b67ee8b954757d7131e9ae3
Juhee,kim <withjh1215@naver.com>
2016-11-21 10:54:33 +0900

    1. DAO 작업 - 전자결재 -> 결재라인 수정

--- src/main/java/kr/or/epm/DAO/DraftDAO.java
+++ src/main/java/kr/or/epm/DAO/DraftDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 import kr.or.epm.VO.Break;
 import kr.or.epm.VO.Cooperation;
 import kr.or.epm.VO.Draft;
+import kr.or.epm.VO.Draft_line;
 import kr.or.epm.VO.Office;
 
 /**
@@ -21,7 +22,10 @@ public interface DraftDAO {
 	//현재 로그인한 사원에게 온 전자결재 조회 ( 수신결재함 조회 )
 	public List<Draft> selectDraft_rec(String emp_no);
 	
-	//승인 처리
+	//결재 라인 별 승인 처리
+	public int approveDraftLine(Draft dto);
+	
+	//결재 문서 별 승인 처리(------수정중-------)
 	public int approveDraft(Draft dto);
 	
 	//수신된 전자결재문서 상세보기(대외발신공문)
@@ -39,8 +43,12 @@ public interface DraftDAO {
 	//수신결재함에서 삭제
 	public int deleteDraft(Draft dto);
 	
-	//결재라인
-	
 	//송신결재함 조회
 	public List<Draft> selectDraft(String emp_no); 
+	
+	//결재라인 진행 현황 
+	public Draft_line selectDraftLine(String draft_no);
+	
+	//결재라인에서  내가 포함된 미승인 결제들의 draft_no 목록 불러오기(내 차례인것만)
+	public List<String> selectDraftLine_myturn(String emp_no);
 }
