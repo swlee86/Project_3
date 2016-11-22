febd12be913381d43668b6612a7a381db60f4301
하재현 <gifmgo@naver.com>
2016-11-22 22:09:36 +0900

    충돌 해결후 소스 원복 완료 - TaskService

--- src/main/java/kr/or/epm/Service/TaskService.java
+++ src/main/java/kr/or/epm/Service/TaskService.java
@@ -126,11 +126,10 @@ public class TaskService {
 
 	}
 	
-	//업무 요청 > 수신 서비스
-	public List<Task> selectTask_rec(String emp_no){
+	//업무 요청, 일지 > 수신 서비스
+	public List<Task> selectTask_rec(String emp_no, String cg_no){
 		System.out.println("selectTask 서비스 : "+emp_no);
 		//업무 요청 페이지
-		String cg_no = "1";
 		TaskDAO taskDAO = sqlsession.getMapper(TaskDAO.class);
 		
 		List<Task> selectList = taskDAO.selectTask_rec(emp_no, cg_no);
@@ -172,6 +171,17 @@ public class TaskService {
 		return serviceResultList;
 	}
 	
+	//업무 요청 > 송신 
+
+	public int countTask(String cg_no) {
+		
+		System.out.println("업무 글 개수 구하기");
+		
+		TaskDAO dao = sqlsession.getMapper(TaskDAO.class);
+		int count = dao.countTask(cg_no);
+		
+		return count;
+	};
 	
 	//승인 여부 선택시 호출 되는 서비스 메서드
 	public int approval(String approval,String task_no){
